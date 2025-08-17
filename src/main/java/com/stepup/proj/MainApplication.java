9217bc2f10baf585eaaab545a197f1c4735ac512
Maxim Hazanov <mkhazanov@inno.tech>
2025-08-17 22:01:58 +0300

    Changed logic Added Box class

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,6 +3,14 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("1111");
+        System.out.println("1001");
+
+        System.out.println("22222");
+        System.out.println("33333");
+        System.out.println("4444");
+    }
+
+    public static void doSomething() {
+        System.out.println("1");
     }
 }
