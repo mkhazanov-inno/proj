e7856a995771887b8668400621ea207ce26ddca1
Maxim Hazanov <mkhazanov@inno.tech>
2025-08-17 22:15:34 +0300

    TASK-124 Bug fixes and improvements

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -2,7 +2,7 @@ package com.stepup.proj;
 
 public class MainApplication {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("----");
         System.out.println("1001");
 
         System.out.println("22222");
@@ -11,8 +11,8 @@ public class MainApplication {
     }
 
     public static void doSomething() {
+        System.out.println("0");
         System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
+        System.out.println("1");
     }
 }
